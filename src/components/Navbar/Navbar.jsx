fcaad9545ee708834145c64fb0d06e7a7048765d
Hugo Quispe Chavez <hugo.quispe12359@gmail.com>
2023-11-06 13:57:53 -0500

    🔥🔥🔥

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,10 +27,10 @@ function Navbar(props) {
                         Quality
                     </span>
                     <ul className={`links${clicked && (scrollX < 650) ? 'active' : ''}`}>
-                        <li><a href="home">Home</a></li>
-                        <li><a href="about">About</a></li>
-                        <li><a href="service">Service</a></li>
-                        <li><a href="contact">Contact</a></li>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Service</a></li>
+                        <li><a href="/">Contact</a></li>
                     </ul>
                     <img onClick={handleClick} src={burger} className="navbar-logo" alt="React logo" />
                 </div>
