21a57f427d7fe2884df86bbfabe5266d5b70f009
Hugo Quispe Chavez <hugo.quispe12359@gmail.com>
2023-12-14 04:49:05 +0100

    footer finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,17 +7,24 @@ import Linkedin from '../../assets/SocialNetwork/linkedin.svg'
 
 
 function Footer() {
+
+    const FACEBOOK_LINK = "https://www.facebook.com/TopQualitySAC?locale=es_LA"
+    const INSTAGRAM_LINK = "https://www.instagram.com/aracelly_m_a"
+    const YOUTUBE_LINK = "https://www.facebook.com/aracelly.municoalfaro?locale=es_LA"
+    const WHATSAPP_LINK = "https://api.whatsapp.com/send/?phone=51951513366&text&type=phone_number&app_absent=0"
+    const LINCKEDIN_LINK = "https://www.linkedin.com/in/aracelly-mu%C3%B1ico-alfaro-515b44184/"
+
     return (
         <>
             <section className="footer">
                 <div className="footer-content">
                     <h2 className='footer-content-title'>Top Quality Consulting 2023 ©</h2>
                     <div className="footer-content-socialnetwork">
-                        <img src={Facebook} />
-                        <img src={Instagram} />
-                        <img src={Youtube} />
-                        <img src={Whatsapp} />
-                        <img src={Linkedin} />
+                        <a href={FACEBOOK_LINK} target="_blank"><img src={Facebook} /></a>
+                        <a href={INSTAGRAM_LINK} target="_blank"><img src={Instagram} /></a>
+                        <a href={YOUTUBE_LINK} target="_blank"><img src={Youtube} /></a>
+                        <a href={WHATSAPP_LINK} target="_blank"><img src={Whatsapp} /></a>
+                        <a href={LINCKEDIN_LINK} target="_blank"><img src={Linkedin} /></a>
                     </div>
                     <h2 className='footer-content-email'>informes@topquality-consulting.com</h2>
                 </div>
